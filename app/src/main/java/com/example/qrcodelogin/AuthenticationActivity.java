14938a1b4c6059b46304ae460d4207a1615d542e
ChoiChangYong <34606508+ChoiChangYong@users.noreply.github.com>
2019-08-09 16:26:16 +0900

    테스트 중 일부 수정

--- app/src/main/java/com/example/qrcodelogin/AuthenticationActivity.java
+++ app/src/main/java/com/example/qrcodelogin/AuthenticationActivity.java
@@ -58,11 +58,15 @@ public class AuthenticationActivity extends AppCompatActivity {
         System.out.println("[openDeepLink] qrcodeSession : "+qrcodeSession);
 
         if (QRLOGIN_DEEP_LINK.equals(path)) {
-            verifyUserSession();
+            validationQRToken();
         }
     }
 
     private void validationQRToken(){
+        SharedPreferences mPrefs = getSharedPreferences("userSession", MODE_PRIVATE);
+        userSession = mPrefs.getString("userSession", "empty");
+        System.out.println("userSession : "+userSession);
+
         Response.Listener<String> responseListener = new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -77,22 +81,23 @@ public class AuthenticationActivity extends AppCompatActivity {
                                 .setPositiveButton("확인", null);
                         alertDialog = builder.create();
                         alertDialog.show();
-                    } else if (result.equals("0")) {
+                    } else if (result.equals("2")) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(AuthenticationActivity.this);
                         builder.setMessage("만료된 QR코드입니다.\n웹페이지에서 다시 발급받아주세요!")
                                 .setPositiveButton("확인", null);
-//                        new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface dialog, int id) {
-//                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                                intent.putExtra("id", id);
-//                                intent.putExtra("name", name);
-//                                startActivity(intent);
-//                                finish();
-//                            }
-//                        }
                         alertDialog = builder.create();
                         alertDialog.show();
-
+                    } else if (result.equals("3")) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(AuthenticationActivity.this);
+                        builder.setMessage("QR코드 로그인 실패!")
+                                .setPositiveButton("확인", null);
+                        alertDialog = builder.create();
+                        alertDialog.show();
+                    } else if (result.equals("0")) {
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        intent.putExtra("redirectQrLogin", redirectQrLogin);
+                        startActivity(intent);
+                        finish();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -104,70 +109,6 @@ public class AuthenticationActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(AuthenticationActivity.this);
         queue.add(qrTokenValidationRequest);
     }
-
-    private void verifyUserSession(){
-        SharedPreferences mPrefs = getSharedPreferences("userSession", MODE_PRIVATE);
-        userSession = mPrefs.getString("userSession", "empty");
-        System.out.println("userSession : "+userSession);
-
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    System.out.println("/user-token/validation response : " + response);
-                    JSONObject jsonResponse = new JSONObject(response);
-
-                    String result = jsonResponse.getString("result");
-
-                    if (result.equals("1")) {
-                        getUserInfoBySessionID(userSession);
-                    } else if (result.equals("0")) {
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        intent.putExtra("redirectQrLogin", redirectQrLogin);
-                        startActivity(intent);
-                        finish();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        UserSessionVerificationRequest userSessionVerificationRequest = new UserSessionVerificationRequest(userSession, responseListener);
-        RequestQueue queue = Volley.newRequestQueue(AuthenticationActivity.this);
-        queue.add(userSessionVerificationRequest);
-    }
-
-    private void getUserInfoBySessionID(String userSession){
-        Response.Listener<JSONObject> responseListener = new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    System.out.println("/users/:sessionID response : " + response);
-                    JSONObject jsonResponse = response;
-
-                    String result = jsonResponse.getString("result");
-                    if (result.equals("1")) {
-                        id = jsonResponse.getString("id");
-                        name = jsonResponse.getString("name");
-                        validationQRToken();
-                    } else if (result.equals("0")) {
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        intent.putExtra("redirectQrLogin", redirectQrLogin);
-                        startActivity(intent);
-                        finish();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        String url = "http://172.20.51.188:3000/users/"+userSession;
-        CommonGetHttpRequest commonGetHttpRequest = new CommonGetHttpRequest(Request.Method.GET, url, null, responseListener, null);
-        RequestQueue queue = Volley.newRequestQueue(AuthenticationActivity.this);
-        queue.add(commonGetHttpRequest);
-    }
-
     @Override
     protected void onStop() {
         super.onStop();
